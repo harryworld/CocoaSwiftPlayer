38d8dba432bbc2767f7d45e1f6f59e534c07bb8d
Harry Ng <harryworld@gmail.com>
2016-01-31 21:45:19 +0800

    Create basic folder structure

---
+++ CocoaSwiftPlayer/Models/Song.swift
@@ -0,0 +1,13 @@
+//
+//  Song.swift
+//  CocoaSwiftPlayer
+//
+//  Created by Harry Ng on 31/1/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+class Song: NSObject {
+
+}
